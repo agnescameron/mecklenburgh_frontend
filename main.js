3530d19a0f37ab78554c42abb260c1872aec1707
Agnes Cameron <agnesfcameron@gmail.com>
2021-09-19 19:50:33 +0100

    gets park info

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 import SVG from './assets/svg.min.js'
 
-let draw = SVG().addTo('body').size(300, 300)
-let rect = draw.rect(100, 100).attr({ fill: '#f06' })
+let draw = SVG().addTo('#park').size(2100, 1650)
+let circle = draw.circle(100).move(100, 50).attr({ fill: 'none', stroke: "black" })
+
+async function park_listener() {
+  const res = await fetch('http://localhost:8888/park')
+  const park = await res.json()
+
+  console.log(park)
+
+}
+
+window.setInterval( function() {
+  park_listener()
+}, 1000)
