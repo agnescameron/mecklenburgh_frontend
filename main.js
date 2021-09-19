d97e80d2ce879b926a9875339cb2295bc3953442
Agnes Cameron <agnesfcameron@gmail.com>
2021-09-19 22:23:39 +0100

    draws park

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 import SVG from './assets/svg.min.js'
+import $ from './assets/jquery.min.js'
 
-let draw = SVG().addTo('#park').size(2100, 1650)
-let circle = draw.circle(100).move(100, 50).attr({ fill: 'none', stroke: "black" })
+// const draw = SVG().addTo('#park').size(2100, 1650)
+let trees = []
+	const draw = SVG().addTo('#park').size(800, 650)
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index);
+  }
+}
+
+async function drawPlant(plant, index) {
+	trees[index] = draw.circle(Number(plant.radius)).move(Number(plant.x_pos), Number(plant.y_pos))
+			.attr({ fill: 'none', stroke: "black" })
+}
+
+async function redraw_park(park) {
+	await asyncForEach(park.plants, drawPlant)
+
+
+	// park.plants.forEach( tree => { 
+	// 	console.log(tree.radius, tree.x_pos, tree.y_pos);
+	// 	;
+	// } )
+		
+}
 
 async function park_listener() {
   const res = await fetch('http://localhost:8888/park')
   const park = await res.json()
-
-  console.log(park)
-
+  await redraw_park(park)
 }
 
 window.setInterval( function() {
   park_listener()
-}, 1000)
+}, 3000)
