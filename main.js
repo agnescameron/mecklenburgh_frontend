5a9a72fd348c866234fb56e10ea97f6bf4ea8d90
Agnes Cameron <agnesfcameron@gmail.com>
2021-09-25 06:08:50 +0100

    renders park csvs

--- main.js
+++ main.js
@@ -11,10 +11,11 @@ async function asyncForEach(array, callback) {
 }
 
 async function drawPlant(plant, index) {
-	svgs[index] = draw.circle(Number(plant.radius)).move(Number(plant.x_pos), Number(plant.y_pos))
-			.attr({ fill: 'none', stroke: "black" })
-			.data('info', plant.description)
-			.click( function() { console.log(this.data('info')) })
+	svgs[index] = draw.circle(Number(plant.radius))
+		.move(Number(plant.x_pos), Number(plant.y_pos))
+		.attr({ fill: 'none', stroke: "black" })
+		.data('info', plant.description)
+		.click( function() { console.log(this.data('info')) })
 }
 
 async function redraw_park(park) {
@@ -23,11 +24,18 @@ async function redraw_park(park) {
 }
 
 async function park_listener() {
-  const res = await fetch('http://localhost:8888/park')
-  const park = await res.json()
-  await redraw_park(park)
+	const res = await fetch('http://localhost:8888/park')
+	const park = await res.json()
+	await redraw_park(park)
 }
 
-window.setInterval( function() {
-  park_listener()
-}, 1000)
+$( document ).ready( function() {
+	// var bed  = draw.use('elementId', 'ed.svg')
+	// var bed  = draw.use('elementId', 'path/to/file.svg')
+
+	park_listener().then( () => {
+		window.setInterval( function() {
+			park_listener()
+		}, 1000)
+	});
+});
